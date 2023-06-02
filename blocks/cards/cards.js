1a13b2bacb2cd1c5947659d972fa6affc816ec1f
Sergino Martina <sergino.martina@gmail.com>
2023-06-02 15:01:52 +0200

    cards object

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,8 +1,11 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
-let cardsContainer = block;
+
 
 export default function decorate(block) {
+  let cardsContainer = block;
+  console.log(cardsContainer);
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
