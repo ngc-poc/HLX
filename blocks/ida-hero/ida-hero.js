1423da2cc73bb0751f50ee32b16773da5fc1f834
Sergino Martina <sergino.martina@gmail.com>
2023-06-02 12:03:08 +0200

    ida-hero styling and img

---
+++ blocks/ida-hero/ida-hero.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var elem = document.createElement("img");
+    elem.setAttribute("src", "https://www.weareida.digital/content/dam/ida/artwork/one-line.png");
+    elem.className = "one-line";
+    document.querySelector('.ida-hero').appendChild(elem);
+  }
