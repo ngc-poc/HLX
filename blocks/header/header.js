b8eaea71380f32b3b2dfa453f08a26511da7fe5e
Sergino Martina <sergino.martina@gmail.com>
2023-06-02 11:42:46 +0200

    add font import

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -143,3 +143,29 @@ export default async function decorate(block) {
     block.append(navWrapper);
   }
 }
+
+
+// font loader Ubuntu and Open Sans
+if(!document.getElementById('font-preloader')) {
+  var link = document.createElement('link');
+  link.id = 'font-preloader';
+  link.rel = 'preconnect';
+  link.href = 'https://fonts.googleapis.com';
+  document.head.appendChild(link);
+}
+
+if(!document.getElementById('font-preconnect')) {
+  var link = document.createElement('link');
+  link.id = 'font-preconnect';
+  link.crossOrigin = 'anonymous';
+  link.href = 'https://fonts.gstatic.com';
+  document.head.appendChild(link);
+}
+
+if(!document.getElementById('fonts')) {
+  var link = document.createElement('link');
+  link.id = 'fonts';
+  link.rel = 'stylesheet'
+  link.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap';
+  document.head.appendChild(link);
+}
